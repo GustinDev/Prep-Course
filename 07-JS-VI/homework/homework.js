7a452768a24ff5b9429895e5a9211cf501c88190
Juan Gustin <juandgustin@gmail.com>
2022-10-16 20:01:07 -0500

    Homework - JS6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,21 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
+  //Nosotros le pasamos los numeros a la función que ejecuta la operación.
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +26,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  sumaIntegers = 0;
+  numeros.forEach((numero) => {
+    sumaIntegers = sumaIntegers + numero;
+  });
+  cb(sumaIntegers);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach((item) => {
+    cb(item);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +47,23 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  let nuevoArray = array.map(function creador(valor) {
+    return cb(valor);
+  });
+  return nuevoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let nuevoArray = [];
+  array.forEach((elemento) => {
+    if (elemento.charAt(0) === 'a' || elemento.charAt(0) === 'A') {
+      nuevoArray.push(elemento);
+    }
+  });
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
@@ -53,5 +76,5 @@ module.exports = {
   sumarArray,
   forEach,
   map,
-  filter
+  filter,
 };
